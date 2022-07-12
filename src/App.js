b12f7f63b5d1ce3673ae50581ad6792fc5ec83ac
renderGUI <90111307+renderGUI@users.noreply.github.com>
2022-07-12 11:06:31 -0400

    Style menu screen

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import classes from "./App.module.scss";
+
 const App = () => {
   return (
-    <div>
-      <p>App</p>
+    <div className={classes.container}>
+      <h2 className={classes.totalTime}>17:00</h2>
+
+      <div className={classes.subcontainer}>
+        <button className={classes.optionsButton}>3:00</button>
+        <button className={classes.goButton}>GO</button>
+        <button className={classes.optionsButton}>0:30</button>
+      </div>
+
+      <div className={classes.subcontainer}>
+        <button className={classes.optionsButton}>x5</button>
+      </div>
     </div>
   );
 };
